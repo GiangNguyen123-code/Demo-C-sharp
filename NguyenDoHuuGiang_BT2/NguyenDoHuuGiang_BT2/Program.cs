be7158fd43bd6b9da30928c8849b297433f87a5b
Nguyen Do Huu Giang <nguyendohuugiang2006@gmail.com>
2026-01-29 08:11:06 +0700

    nopbai

---
+++ NguyenDoHuuGiang_BT2/NguyenDoHuuGiang_BT2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+Console.InputEncoding = System.Text.Encoding.UTF8;
+Console.Write("Nhập chuỗi bất kỳ: ");
+string input = Console.ReadLine();
+
+if (input == null)
+{
+    Console.WriteLine("Số từ: 0");
+    return;
+}
+
+string[] words = input.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+Console.WriteLine($"Số từ trong chuỗi: {words.Length}");
---
+++ NguyenDoHuuGiang_BT3/NguyenDoHuuGiang_BT3/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+Console.InputEncoding = System.Text.Encoding.UTF8;
+Console.Write("Nhập chuỗi: ");
+string input = Console.ReadLine();
+
+if (input == null)
+{
+    Console.WriteLine("Kết luận: Không đối xứng");
+    return;
+}
+string temp = input.Replace(" ", "").ToLower();
+bool laDoiXung = true;
+
+for (int i = 0; i < temp.Length / 2; i++)
+{
+    if (temp[i] != temp[temp.Length - 1 - i])
+    {
+        laDoiXung = false;
+        break;
+    }
+}
+
+Console.WriteLine(laDoiXung ? "=> Đây là chuỗi đối xứng" : "=> Không phải chuỗi đối xứng");
